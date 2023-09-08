2294dc046604da5e1694380a8ae60531cdfb6f7a
dan-candeira <danielbarr16@gmail.com>
2023-09-08 19:12:12 -0300

    feat: update project structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,20 @@
-module.exports = config => {
-	// Tell 11ty to use the .eleventyignore and ignore our .gitignore file
-	config.setUseGitIgnore(false);
+const litPlugin = require('@lit-labs/eleventy-plugin-lit');
 
-	config.addPassthroughCopy('./src/fonts');
-	config.addPassthroughCopy('./src/images');
+module.exports = eleventyConfig => {
+	// Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+	eleventyConfig.setUseGitIgnore(false);
+
+	eleventyConfig.addPassthroughCopy('./src/fonts');
+	eleventyConfig.addPassthroughCopy('./src/images');
 
 	// config.addFilter('md', require('./src/filters/md.js'));
+  eleventyConfig.addPlugin(litPlugin, {
+    mode: 'worker',
+    componentModules: [
+    //   'js/demo-greeter.js',
+    //   'js/other-component.js',
+    ],
+  });
 
 	return {
 		markdownTemplateEngine: 'njk',
